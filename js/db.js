cd2630b462f35ce63e0e0f7c22b93e0af9b801b1
Arthur Silva Dantas <arthur.dantas@estudante.ifms.edu.br>
2023-09-28 08:26:02 -0300

    Atualizando Anotação - Add function

--- js/db.js
+++ js/db.js
@@ -25,7 +25,7 @@ async function createDB() {
 
 window.addEventListener("DOMContentLoaded", async (event) => {
   createDB();
-  document.getElementById("btnCadastro").addEventListener("click", adicionarAnotacao);
+  document.getElementById("btnCadastro").addEventListener("click", adicionarOuAtualizarAnotacao);
   document.getElementById("btnCarregar").addEventListener("click", buscarTodasAnotacoes);
   document.getElementById("btnBuscar").addEventListener("click", buscarAnotacaoPorTitulo);
 });
@@ -45,25 +45,7 @@ async function buscarTodasAnotacoes() {
 
   if (value.length > 0) {
     value.forEach(item => {
-      const card = document.createElement('div');
-      card.classList.add('card'); 
-
-      const tituloElement = document.createElement('p');
-      const descricaoElement = document.createElement('p');
-      const categoriaElement = document.createElement('p');
-
-      tituloElement.textContent = `Título: ${item.titulo}`;
-      descricaoElement.textContent = `Descrição: ${item.descricao}`;
-      categoriaElement.textContent = `Categoria: ${item.categoria}`
-
-      const removerButton = document.createElement('button');
-      removerButton.textContent = 'Remover';
-      removerButton.addEventListener('click', () => excluirAnotacao(item.titulo));
-      
-      card.appendChild(tituloElement);
-      card.appendChild(descricaoElement);
-      card.appendChild(categoriaElement);
-      card.appendChild(removerButton);
+      const card = criarCartaoAnotacao(item);
       listagemDiv.appendChild(card);
     });
   } else {
@@ -71,33 +53,119 @@ async function buscarTodasAnotacoes() {
   }
 }
 
-async function adicionarAnotacao() {
+function criarCartaoAnotacao(anotacao) {
+  const card = document.createElement('div');
+  card.classList.add('card'); 
+
+  const tituloElement = document.createElement('p');
+  const descricaoElement = document.createElement('p');
+  const categoriaElement = document.createElement('p');
+
+  tituloElement.textContent = `Título: ${anotacao.titulo}`;
+  descricaoElement.textContent = `Descrição: ${anotacao.descricao}`;
+  categoriaElement.textContent = `Categoria: ${anotacao.categoria}`;
+
+  const editarButton = criarBotao("Editar", () => editarAnotacao(anotacao));
+  const removerButton = criarBotao("Remover", () => excluirAnotacao(anotacao.titulo));
+
+  card.appendChild(tituloElement);
+  card.appendChild(descricaoElement);
+  card.appendChild(categoriaElement);
+  card.appendChild(editarButton);
+  card.appendChild(removerButton);
+
+  return card;
+}
+
+//Criando componente para ser passado no card
+function criarBotao(texto, onClick) {
+  const button = document.createElement('button');
+  button.textContent = texto;
+  button.addEventListener('click', onClick);
+  return button;
+}
+
+function editarAnotacao(anotacao) {
+  const tituloInput = document.getElementById("titulo");
+  const descricaoInput = document.getElementById("descricao");
+  const categoriaInput = document.getElementById("categoria");
+  const dataInput = document.getElementById("data");
+
+  tituloInput.value = anotacao.titulo;
+  descricaoInput.value = anotacao.descricao;
+  categoriaInput.value = anotacao.categoria;
+  dataInput.value = anotacao.data;
+
+  const cadastrarButton = document.getElementById("btnCadastro");
+  cadastrarButton.textContent = "Atualizar";
+  cadastrarButton.removeEventListener("click", adicionarOuAtualizarAnotacao);
+  cadastrarButton.addEventListener("click", () => atualizarAnotacao(anotacao.titulo));
+}
+
+async function atualizarAnotacao(tituloOriginal) {
   const titulo = document.getElementById("titulo").value;
   const categoria = document.getElementById("categoria").value;
   const descricao = document.getElementById("descricao").value;
   const data = document.getElementById("data").value;
-
-  if (db === undefined) {
-    showResult("O banco de dados está fechado.");
-    return;
-  }
 
   if (!titulo || !categoria || !descricao || !data) {
     showResult("Preencha todos os campos.");
     return;
   }
 
+  if (db === undefined) {
+    showResult("O banco de dados está fechado.");
+    return;
+  }
+
   const tx = await db.transaction("anotacao", "readwrite");
   const store = tx.objectStore("anotacao");
-  
+
   try {
+    await store.delete(tituloOriginal);
     await store.add({ titulo, categoria, descricao, data });
     await tx.done;
     limparCampos();
-    buscarTodasAnotacoes(); 
-    showResult("Anotação adicionada com sucesso!");
+    buscarTodasAnotacoes();
+    showResult("Anotação atualizada com sucesso!");
+
+    const cadastrarButton = document.getElementById("btnCadastro");
+    cadastrarButton.textContent = "Cadastrar";
+    cadastrarButton.removeEventListener("click", atualizarAnotacao);
+    cadastrarButton.addEventListener("click", adicionarOuAtualizarAnotacao);
   } catch (error) {
-    console.error("Erro ao adicionar anotação:", error);
+    console.error("Erro ao atualizar anotação:", error);
+    tx.abort();
+  }
+}
+
+async function adicionarOuAtualizarAnotacao() {
+  const titulo = document.getElementById("titulo").value;
+  const categoria = document.getElementById("categoria").value;
+  const descricao = document.getElementById("descricao").value;
+  const data = document.getElementById("data").value;
+
+  if (!titulo || !categoria || !descricao || !data) {
+    showResult("Preencha todos os campos.");
+    return;
+  }
+
+  if (db === undefined) {
+    showResult("O banco de dados está fechado.");
+    return;
+  }
+
+  const tx = await db.transaction("anotacao", "readwrite");
+  const store = tx.objectStore("anotacao");
+
+  try {
+    await store.put({ titulo, categoria, descricao, data });
+    await tx.done;
+    limparCampos();
+    buscarTodasAnotacoes();
+    showResult("Anotação adicionada ou atualizada com sucesso!");
+  } catch (error) {
+    console.error("Erro ao adicionar ou atualizar anotação:", error);
     tx.abort();
   }
 }
@@ -115,7 +183,7 @@ async function excluirAnotacao(titulo) {
     await store.delete(titulo);
     await tx.done;
     showResult("Anotação excluída com sucesso!");
-    buscarTodasAnotacoes(); 
+    buscarTodasAnotacoes();
   } catch (error) {
     console.error("Erro ao excluir anotação:", error);
     tx.abort();
@@ -145,25 +213,7 @@ async function buscarAnotacaoPorTitulo() {
       const listagemDiv = document.querySelector('.listagem');
       listagemDiv.innerHTML = '';
 
-      const card = document.createElement('div');
-      card.classList.add('card');
-
-      const tituloElement = document.createElement('p');
-      const descricaoElement = document.createElement('p');
-      const categoriaElement = document.createElement('p');
-
-      tituloElement.textContent = `Título: ${anotacao.titulo}`;
-      descricaoElement.textContent = `Descrição: ${anotacao.descricao}`;
-      categoriaElement.textContent = `Categoria: ${anotacao.categoria}`
-
-      const removerButton = document.createElement('button');
-      removerButton.textContent = 'Remover';
-      removerButton.addEventListener('click', () => excluirAnotacao(anotacao.titulo));
-
-      card.appendChild(tituloElement);
-      card.appendChild(descricaoElement);
-      card.appendChild(categoriaElement);
-      card.appendChild(removerButton);
+      const card = criarCartaoAnotacao(anotacao);
       listagemDiv.appendChild(card);
 
       showResult("Anotação encontrada.");
